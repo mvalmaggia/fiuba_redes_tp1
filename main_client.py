1e10d5e452a6016e367677f09f2e24571c015b65
Alexis Herrera <alexysaguilar@gmail.com>
2024-04-14 16:28:21 -0300

    definición de protocolo

--- main_client.py
+++ main_client.py
@@ -1,11 +1,14 @@
 import socket
+import time
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(b"Hola", (UDP_IP, UDP_PORT))
+    while True:
+        sock.sendto(b"Hola", (UDP_IP, UDP_PORT))
+        time.sleep(1)
 
 
 if __name__ == "__main__":
