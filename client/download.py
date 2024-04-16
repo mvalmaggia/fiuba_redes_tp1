2b4b00082c3a48b3839d0ea30fc5f7b45026c098
ramaxx95 <ramilozano@live.com.ar>
2024-04-16 14:54:15 -0300

    parseo de argumentos completo

--- client/download.py
+++ client/download.py
@@ -1,7 +1,7 @@
 import sys
 
-VERBOSE = 0
-QUIET = 1
+# Por defecto
+verbose = True
 
 
 # Formato linea de comando:
@@ -14,15 +14,34 @@ QUIET = 1
 # -n:      Nombre del archivo a bajar
 # NOTA: SON TODAS OPCIONALES ESTAS OPCIONES
 def main():
+    global verbose
+    print('[DEBUG] verbose = ', verbose)
     args = parse_args()
-    if args[0] == '-h' or args[0] == '--help':
+    print('[DEBUG] args = ', args)
+    if args[0]:
         print_help()
+        exit()
+    elif not args[1]:
+        verbose = False
+        print('[DEBUG] verbose = ', verbose)
 
 
 def parse_args():
-    args = []
+    # [-h, -v|-q, -H, -p, -d, -n]
+    args = [False, True, False, False, False, False]
     for i in range(1, len(sys.argv)):
-        args.append(sys.argv[i])
+        if sys.argv[i] == '-h' or sys.argv[i] == '--help':
+            args[0] = True
+        elif sys.argv[i] == '-q' or sys.argv[i] == '--quiet':
+            args[1] = False
+        elif sys.argv[i] == '-H' or sys.argv[i] == '--host':
+            args[2] = True
+        elif sys.argv[i] == '-p' or sys.argv[i] == '--port':
+            args[3] = True
+        elif sys.argv[i] == '-d' or sys.argv[i] == '--dst':
+            args[4] = True
+        elif sys.argv[i] == '-n' or sys.argv[i] == '--name':
+            args[5] = True
 
     return args
 
