a0546f3b7209086eef67e0bc68096e1e743f0916
mvalmaggia <mvalmaggia@fi.uba.ar>
2024-04-24 23:04:56 -0300

    created general layout for server

--- main_server.py
+++ main_server.py
@@ -1,7 +1,26 @@
 import socket
+import threading
+import pickle
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+
+def handle_message(data, addr):
+    print("received message: ", data)
+    print("from: ", addr)
+
+    received_packet = pickle.loads(data)
+
+    if received_packet.get_is_query() == True:
+        # check if is upload or download
+        print("processing query")
+
+    elif received_packet.get_fin() == True:
+        # send ack for ending connection
+        print("file transfer finished")
+
+    else: 
+        #aca tengo data si o si     
 
 # Servidor
 def main():
@@ -9,9 +28,9 @@ def main():
     sock.bind((UDP_IP, UDP_PORT))
     while True:
         data, addr = sock.recvfrom(1024)
-        print("received message: ", data)
-        print("from: ", addr)
-        sock.sendto(data, addr)
+
+        thread = threading.Thread(target=handle_message, args=(data, addr))
+        thread.start()
 
 
 if __name__ == "__main__":
