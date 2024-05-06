b2c761eb4c8cb3cd8d70a7406808e91d39833996
ramaxx95 <ramilozano@live.com.ar>
2024-05-06 15:01:23 -0300

    primer draft para 'receive_packet' en gobackn

--- lib/retransmit_strategy.py
+++ lib/retransmit_strategy.py
@@ -13,7 +13,7 @@ class RetransmitStrategyInterface(ABC):
         pass
 
     @abstractmethod
-    def receive_packet(self):
+    def receive_packet(self, recv_sock: socket, sender_addr):
         pass
 
 
@@ -22,13 +22,14 @@ class StopNWait(RetransmitStrategyInterface):
     def send_packets(self, sender_sock: socket, receiver_addr, file):
         pass
 
-    def receive_packet(self):
+    def receive_packet(self, recv_sock: socket, sender_addr):
         pass
 
 
 class GoBackN(RetransmitStrategyInterface):
     base_seq_num: int
     next_seq_num: int
+    expected_seq_num: int
     last_ack_pkt: int
     repeated_acks: int
     window: int
@@ -38,7 +39,8 @@ class GoBackN(RetransmitStrategyInterface):
         self.repeated_acks = 0
 
     def send_packets(self, sender_sock: socket, receiver_addr, file):
-        self.base_seq_num, self.next_seq_num = 1, 1
+        self.base_seq_num = 1
+        self.next_seq_num = 1
         packets = extract_packets_from(file)
         self.last_ack_pkt = 0
 
@@ -46,7 +48,7 @@ class GoBackN(RetransmitStrategyInterface):
         timeout = 5.0
 
         while True:
-            if self.last_ack_pkt == packets.__sizeof__():
+            if self.last_ack_pkt == len(packets):
                 break
 
             # evento: timeout
@@ -58,7 +60,7 @@ class GoBackN(RetransmitStrategyInterface):
                     sender_sock.sendto(buf, receiver_addr)
 
             # deja de enviar paquetes cuando llega al fin de 'packets'
-            if self.next_seq_num <= packets.__sizeof__():
+            if self.next_seq_num <= len(packets):
 
                 # evento: mandado de paquetes dentro de la ventana
                 if self.next_seq_num < (self.base_seq_num + self.window):
@@ -69,23 +71,56 @@ class GoBackN(RetransmitStrategyInterface):
 
                     if self.base_seq_num == self.next_seq_num:
                         timer.start()
-                    self.next_seq_num = self.next_seq_num + 1
+                    self.next_seq_num += 1
 
             # evento: recibir ack
-            ack_pkt = sender_sock.recvfrom(5000)
-            if ack_pkt.get_ack() == self.last_ack_pkt:
-                self.repeated_acks = self.repeated_acks + 1
+            ack_pkt, receiver_addr = sender_sock.recvfrom(5000)
+            decoded_ack_pkt = pickle.loads(ack_pkt)
+            if decoded_ack_pkt.get_ack() == self.last_ack_pkt:
+                self.repeated_acks += 1
             else:
-                self.last_ack_pkt = ack_pkt.get_ack()
+                self.last_ack_pkt = decoded_ack_pkt.get_ack()
                 self.repeated_acks = 0
-                self.base_seq_num = ack_pkt.get_ack() + 1
+                self.base_seq_num = decoded_ack_pkt.get_ack() + 1
                 if self.base_seq_num == self.next_seq_num:
                     timer.stop()
                 else:
                     timer.reset()
 
-    def receive_packet(self):
-        pass
+        fin_pkt = Packet(self.next_seq_num, True)
+        buffer = pickle.dumps(fin_pkt)
+        sender_sock.sendto(buffer, receiver_addr)
+
+    def receive_packet(self, recv_sock: socket, sender_addr):
+        self.expected_seq_num = 1
+        pkts = []
+
+        while True:
+            recv_pkt, sender_addr = recv_sock.recv(5000)
+            decoded_recv_pkt = pickle.loads(recv_pkt)
+            if decoded_recv_pkt.get_fin():
+                break
+
+            # evento: paquete se recibe exitosamente
+            elif decoded_recv_pkt.get_seq_num() == self.expected_seq_num:
+                pkts.append(decoded_recv_pkt)
+                ack_pkt = Packet(self.expected_seq_num, False)
+                buf = pickle.dumps(ack_pkt)
+                recv_sock.sendto(buf, sender_addr)
+                self.expected_seq_num += 1
+
+            # evento: no recibio paquete -> mandar ack de ultimo paquete recibido
+            else:
+                ack_pkt = Packet(self.expected_seq_num, False)
+                buf = pickle.dumps(ack_pkt)
+                recv_sock.sendto(buf, sender_addr)
+
+        # manda fin de comunicacion
+        fin_pkt = Packet(self.expected_seq_num, True)
+        buf = pickle.dumps(fin_pkt)
+        recv_sock.sendto(buf, sender_addr)
+
+        return pkts
 
 
 def extract_packets_from(file):
