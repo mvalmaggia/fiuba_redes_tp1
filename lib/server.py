d2cc8d4d9f8782c088b842e3c82d1c6441523be4
Alexis Herrera <alexysaguilar@gmail.com>
2024-05-06 22:56:27 -0300

    paso a multihilos por cliente

--- lib/server.py
+++ lib/server.py
@@ -1,10 +1,10 @@
+import queue
 import threading
 from enum import Enum
 
 from lib.packet import Packet
 from lib.sec_num_registry import SecNumberRegistry
-from lib.transmission import send_stop_n_wait, receive, send_file, handle_acks_and_retransmissions
-from lib.window import Window
+from lib.transmission import send_stop_n_wait, receive, send_file
 
 
 class AlgorithmType(Enum):
@@ -17,41 +17,54 @@ class Server:
         # Aqui se guarda el registro del ultimo numero de secuencia enviado a cada cliente
         # Necesita ser thread-safe porque no puede despues de escribir parar a leer, sino que con esto
         # Se puede despachar el ack, registrarlo y luego hacer polling para ver si se recibio el ack
-        self.seq_nums_sent = SecNumberRegistry()
-        self.seq_nums_recv = SecNumberRegistry()
-        self._clients = {}
         self.server_socket = server_socket
+        self.send_lock = threading.Lock()
         self.dir_path = dir_path
         self.algorithm = algorithm
+        self.client_handlers = {}
+        self._clients_pending_upload = {}
+        self.seq_nums_sent = SecNumberRegistry()
+        self.seq_nums_recv = SecNumberRegistry()
 
     def listen(self):
         print('[INFO] Server listo para recibir consultas')
 
         while True:
-            received_packet, client_address = receive(self.server_socket)
-            print('[INFO] Paquete recibido: ', received_packet, ' de ', client_address)
-            if received_packet.ack:
-                self.seq_nums_sent.set_ack(client_address, received_packet.get_seq_num())
+            packet, client_address = receive(self.server_socket)
+            print('[INFO] Paquete recibido: ', packet, ' de ', client_address)
+            if packet.get_ack():
+                self.seq_nums_sent.set_ack(client_address, packet.get_seq_num())
                 continue
-            if self.seq_nums_recv.has(client_address, received_packet.get_seq_num()):
+            if client_address not in self.client_handlers:
+                # Crear una nueva cola para este cliente
+                client_queue = queue.Queue()
+                self.client_handlers[client_address] = client_queue
+                print('[INFO] Nuevo cliente: ', client_address)
+                # Iniciar un nuevo hilo para manejar a este cliente
+                client_thread = threading.Thread(target=self.handle_client, args=(client_address, client_queue))
+                client_thread.start()
+
+            # Enviar el paquete a la cola del cliente
+            self.client_handlers[client_address].put(packet)
+
+    def handle_client(self, client_address, client_queue):
+        while True:
+            packet = client_queue.get()  # Bloquea hasta que hay un paquete en la cola
+            if packet.ack:
+                self.seq_nums_sent.set_ack(client_address, packet.get_seq_num())
+                continue
+            if self.seq_nums_recv.has(client_address, packet.get_seq_num()):
                 print("[INFO] Paquete ya recibido")
                 self.send_ack(client_address)
                 continue
-            self.seq_nums_recv.set_ack(client_address, received_packet.get_seq_num())
-            if received_packet.get_fin():
-                self._clients.pop(client_address, None)
+            self.seq_nums_recv.set_ack(client_address, packet.get_seq_num())
+            if packet.get_fin():
                 self.send_ack(client_address)
                 print('[INFO] Conexion con ', client_address, ' finalizada')
-                continue
+                break
+            self.process_packet(packet, client_address)
 
-            thread = threading.Thread(target=self.handle_message,
-                                      args=(received_packet,
-                                            client_address,
-                                            ))
-            thread.start()
-
-    def handle_message(self, packet: Packet, client_address):
-        print('[INFO] Conexion con ', client_address)
+    def process_packet(self, packet: Packet, client_address):
         if packet.get_is_download_query():
             start_seq_num = 1
             self.send_ack(client_address)
@@ -60,11 +73,10 @@ class Server:
         elif packet.get_is_upload_query():
             file_path = self.dir_path + '/' + packet.get_file_name()
             open(file_path, "w").close()
-            self._clients[client_address] = file_path
+            self._clients_pending_upload[client_address] = file_path
             self.send_ack(client_address)
-        elif client_address in self._clients:
-            # self.clients_pending_upload[client_address] = packet.get_seq_num()
-            file_path = self._clients[client_address]
+        elif client_address in self._clients_pending_upload:
+            file_path = self._clients_pending_upload[client_address]
             self.save_packet_in_file(packet, file_path)
             self.send_ack(client_address)
         else:
@@ -74,17 +86,15 @@ class Server:
         last_ack = self.seq_nums_recv.get_last_ack(client_address)
         print('El ultimo ack enviado fue: ', last_ack)
         ack_packet = Packet(last_ack + 1, ack=True)
-        self.send_server(self.server_socket, client_address, ack_packet)
+        self.send_server(client_address, ack_packet)
 
-    def send_server(self, server_socket, client_address, packet):
-        send_stop_n_wait(server_socket, client_address, packet, lambda seq_num: self.seq_nums_sent.has(client_address, seq_num))
+    def send_server(self, client_address, packet):
+        with self.send_lock:
+            send_stop_n_wait(self.server_socket, client_address, packet,
+                             lambda seq_num: self.seq_nums_sent.has(client_address, seq_num))
 
-    def send_file_server(self, client_address, file_path):
-        initial_sec_num = self.seq_nums_sent.get_last_ack(client_address)
-        send_file(self.server_socket, client_address, file_path, initial_sec_num,
-                  lambda seq_num: self.seq_nums_sent.has(client_address, seq_num))
-
-    def save_packet_in_file(self, packet: Packet, file_path):
+    @staticmethod
+    def save_packet_in_file(packet: Packet, file_path):
         data = packet.get_data()
         with open(file_path, "ab") as file:
             file.write(data)
